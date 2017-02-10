043373f29cf4c202e669ae67ac86846ef88f7610
Sreelakshminarayanan Muthuraj <Sreelakshminarayanan.Muthuraj@cognizant.com>
2017-02-10 16:36:19 -0700

    Included spec here

---
+++ spec/helloWorldSpec.js
@@ -0,0 +1,8 @@
+describe("Hello World Server", function() {
+  it("Dummy test", function() {
+    expect(true).toBe(true);
+  });
+  it("Failing test", function() {
+    expect(true).toBe(true);
+  });
+});
