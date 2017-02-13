c72edd6d07d83cb8db45237c0830a955659714e4
Sreelakshminarayanan Muthuraj <Sreelakshminarayanan.Muthuraj@cognizant.com>
2017-02-13 12:38:07 -0700

    Include WebHooks

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 var express = require('express')
 var app = express()
+var bodyParser = require('body-parser')
 
 app.set('port', (process.env.PORT || 3001))
+// create application/json parser
+var jsonParser = bodyParser.json()
+
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+app.post('/', jsonParser, function (req, res) {
+  if (!req.body) return res.sendStatus(400)
+  console.log("Response is",req.body)
+  res.send(req.body)
+  // create user in req.body
+})
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
