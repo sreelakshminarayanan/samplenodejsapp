5af11e87c0fa35d1629e592742db9b6c0cc96f4d
Sreelakshminarayanan <sreelakshminarayanan@Sreelakshminarayanans-MacBook-Pro.local>
2017-02-13 15:38:31 -0700

    Modified the spec to handle failures

--- spec/helloWorldSpec.js
+++ spec/helloWorldSpec.js
@@ -5,4 +5,7 @@ describe("Hello World Server", function() {
   it("Failing test", function() {
     expect(true).toBe(true);
   });
+  it("Failing test2", function() {
+    expect(true).toBe(false);
+  });
 });
