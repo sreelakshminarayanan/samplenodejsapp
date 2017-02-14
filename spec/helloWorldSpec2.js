35921eee0fc1469112879246eb49e879d26eb03b
Sreelakshminarayanan <sreelakshminarayanan@Sreelakshminarayanans-MacBook-Pro.local>
2017-02-14 10:11:21 -0700

    Included additional spec

---
+++ spec/helloWorldSpec2.js
@@ -0,0 +1,12 @@
+var assert = require('assert');
+describe("Hello World Server2", function() {
+  it("Dummy test2", function() {
+    assert.equal(true,true);
+  });
+  it("Failing test2", function() {
+    assert.equal(true,true);
+  });
+  it("Failing test22", function() {
+    assert.equal(true,true);
+  });
+});
